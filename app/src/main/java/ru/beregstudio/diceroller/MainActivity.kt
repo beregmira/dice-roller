a74418a90820d2a603a32af93a259f2c83d1f7cb
beregmira <beregmira@yandex.ru>
2021-10-10 08:33:48 +0300

    Убрал лишний закомментированный код и обновил комментарии

--- app/src/main/java/ru/beregstudio/diceroller/MainActivity.kt
+++ app/src/main/java/ru/beregstudio/diceroller/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.app.AppCompatActivity
 const val SIX_SIDES: Int = 6 // Количество граней одного кубика
 const val DEFAULT_NUMBER_OF_DICE: Int = 1
 
-// Простое первое приложение на Kotlin
+// Основной экран Activity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,13 +17,9 @@ class MainActivity : AppCompatActivity() {
         val rollNumber: TextView = findViewById(R.id.diceNumber)
         val rollButton: Button = findViewById(R.id.diceButton)
         val seekDice: SeekBar = findViewById(R.id.seekBar)
-//        var seekBarText: TextView = findViewById(R.id.debugProgressBar)
-
-//        seekBarText.text = (seekDice.progress+1).toString() + " dice"
-
         val diceRun = arrayListOf<Dice>()
 
-        //Отрисовка кубика
+        // Отрисовка кубика
         fun roller(numOfDice: Int) {
             var sum = 0
             for (i in 1..numOfDice) {
@@ -37,26 +33,24 @@ class MainActivity : AppCompatActivity() {
                 sum += dice.diceRoll
                 rollNumber.text = sum.toString()
                 println(dice.diceRoll.toString())
-//                println(dice.getRandomDice())
             }
             println("Stop debug")
             diceRun.clear()
         }
         roller(DEFAULT_NUMBER_OF_DICE)
 
-//Клик по кнопке
+        // Клик по кнопке "ROLL"
         rollButton.setOnClickListener {
             layout.removeAllViews()
             roller(seekDice.progress + 1)
         }
 
-//Событие изменения количества кубиков
+        // Событие изменения количества кубиков
         seekDice.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
 
             override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                 layout.removeAllViews()
                 roller(seekBar.progress + 1)
-//                seekBarText.text = (seekDice.progress+1).toString() + " dices"
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar) {}
@@ -64,7 +58,7 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
-    //Получение нового кубика
+    // Получение нового кубика
     private fun getDice(): Dice {
         return Dice(SIX_SIDES, this)
     }
