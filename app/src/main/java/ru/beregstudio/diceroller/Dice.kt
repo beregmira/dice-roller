cef2f41acad3ca222ef79e0ee3e4188f047eabf8
beregmira <beregmira@yandex.ru>
2025-08-09 08:56:54 +0300

    Update libs Optimize Dice class

--- app/src/main/java/ru/beregstudio/diceroller/Dice.kt
+++ app/src/main/java/ru/beregstudio/diceroller/Dice.kt
@@ -6,46 +6,43 @@ import kotlin.random.Random
 
 
 class Dice(private val numSide: Int, private val diceSet: Int, contextParam: Context) {
-    var diceRoll = getRandomDice()
-    var image = ImageView(contextParam)
+    val diceRoll = getRandomDice()
+    val image = ImageView(contextParam)
 
-    private fun getDiceSetOneRandomImage(): Int = when (diceRoll) {
-        1 -> R.drawable.set_1_dice_1
-        2 -> R.drawable.set_1_dice_2
-        3 -> R.drawable.set_1_dice_3
-        4 -> R.drawable.set_1_dice_4
-        5 -> R.drawable.set_1_dice_5
-        else -> R.drawable.set_1_dice_6
-    }
-
-    private fun getDiceSetTwoRandomImage(): Int = when (diceRoll) {
-        1 -> R.drawable.set_2_dice_1
-        2 -> R.drawable.set_2_dice_2
-        3 -> R.drawable.set_2_dice_3
-        4 -> R.drawable.set_2_dice_4
-        5 -> R.drawable.set_2_dice_5
-        else -> R.drawable.set_2_dice_6
-    }
-
-    private fun getDiceSetThreeRandomImage(): Int = when (diceRoll) {
-        1 -> R.drawable.set_3_dice_1
-        2 -> R.drawable.set_3_dice_2
-        3 -> R.drawable.set_3_dice_3
-        4 -> R.drawable.set_3_dice_4
-        5 -> R.drawable.set_3_dice_5
-        else -> R.drawable.set_3_dice_6
-    }
+    private val diceResources = mapOf(
+        1 to listOf(
+            R.drawable.set_1_dice_1,
+            R.drawable.set_1_dice_2,
+            R.drawable.set_1_dice_3,
+            R.drawable.set_1_dice_4,
+            R.drawable.set_1_dice_5,
+            R.drawable.set_1_dice_6
+        ),
+        2 to listOf(
+            R.drawable.set_2_dice_1,
+            R.drawable.set_2_dice_2,
+            R.drawable.set_2_dice_3,
+            R.drawable.set_2_dice_4,
+            R.drawable.set_2_dice_5,
+            R.drawable.set_2_dice_6
+        ),
+        3 to listOf(
+            R.drawable.set_3_dice_1,
+            R.drawable.set_3_dice_2,
+            R.drawable.set_3_dice_3,
+            R.drawable.set_3_dice_4,
+            R.drawable.set_3_dice_5,
+            R.drawable.set_3_dice_6
+        )
+    )
 
     init {
         setDiceImage()
     }
 
     fun setDiceImage() {
-        when (diceSet) {
-            1 -> image.setImageResource(getDiceSetOneRandomImage())
-            2 -> image.setImageResource(getDiceSetTwoRandomImage())
-            else -> image.setImageResource(getDiceSetThreeRandomImage())
-        }
+        val resources = diceResources[diceSet] ?: return
+        image.setImageResource(resources[diceRoll - 1])
         image.contentDescription = diceRoll.toString()
     }
 
